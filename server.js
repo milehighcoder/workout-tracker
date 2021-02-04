89ff6fc40e9afc701cb7cbae521e2becabbd3c28
mykereel <mgmartnz@icloud.com>
2021-02-04 10:55:39 -0700

    added html routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,18 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Express Connections
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+//Mongoose Connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes.js")(app);
 
+// Server Listener
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
